c25b2f93807196b15e96368278ca24b6a8cebda4
RamanTomar90 <raman50203@gmail.com>
2026-04-20 03:32:53 +0530

    fixed connect-mongo session issue

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-if(process.env.NODE_ENV != "production"){
+if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
 
-
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose"); 
+const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
@@ -13,35 +12,32 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 const session = require("express-session");
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-
-const dbUrl = process.env.ATLASDB_URL;
-
 const store = MongoStore.create({
-  mongoUrl: dbUrl,
-  crypto:{
-    secret:process.env.SECRET
+  mongoUrl: process.env.ATLASDB_URL,
+  crypto: {
+    secret: process.env.SECRET
   },
   touchAfter: 24 * 3600,
 });
 
-store.on("error",(err)=>{
+store.on("error", (err) => {
   console.log("Error in MONGO STORE", err);
 });
 
 const sessionOptions = {
   store,
-  secret:process.env.SECRET,
+  secret: process.env.SECRET,
   resave: false,
-  saveUninitialized:true,
-  cookie:{
+  saveUninitialized: false,
+  cookie: {
     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-    maxAge:  7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
   }
 };
@@ -63,30 +59,26 @@ app.use((req, res, next) => {
 });
 
 app.engine("ejs", ejsMate);
-app.set("view engine","ejs");
-app.set("views", path.join(__dirname,"views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
-async function main(){
-  await mongoose.connect(dbUrl);
-}
-
-main()
-.then(()=> console.log("connected to db"))
-.catch((err)=> console.log(err));
+mongoose.connect(process.env.ATLASDB_URL)
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.log(err));
 
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/listings", listingRouter);
 app.use("/", userRouter);
 
-app.use((err,req,res,next)=>{
-  let {statusCode = 500} = err;
-  res.status(statusCode).render("error.ejs",{err});
+app.use((err, req, res, next) => {
+  let { statusCode = 500 } = err;
+  res.status(statusCode).render("error.ejs", { err });
 });
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
   console.log("server is listening on port 8080");
 });
