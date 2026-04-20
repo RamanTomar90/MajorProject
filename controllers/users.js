6db17c474416af4a9af60e72e21f176d74cdc1ef
RamanTomar90 <raman50203@gmail.com>
2026-04-20 06:17:45 +0530

    fix project errors and improve code structure

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,11 @@
 const User = require("../models/user");
 
+// SIGNUP FORM
 module.exports.renderSignUp = (req, res) => {
-    res.render("users/signup.ejs");
+    return res.render("users/signup");
 };
 
+// SIGNUP
 module.exports.signup = async (req, res, next) => {
     try {
         let { username, email, password } = req.body;
@@ -16,30 +18,34 @@ module.exports.signup = async (req, res, next) => {
                 return next(err);
             }
             req.flash("success", "Welcome to WanderLust!");
-            res.redirect("/listings");
+            return res.redirect("/listings");
         });
+
     } catch (e) {
         req.flash("error", e.message);
-        res.redirect("/signup");
+        return res.redirect("/signup");
     }
 };
 
+// LOGIN FORM
 module.exports.renderLoginForm = (req, res) => {
-    res.render("users/login.ejs");
+    return res.render("users/login");
 };
 
+// LOGIN
 module.exports.login = async (req, res) => {
     req.flash("success", "Welcome back to WanderLust!");
     let redirectUrl = res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
+    return res.redirect(redirectUrl);
 };
 
+// LOGOUT
 module.exports.logout = (req, res, next) => {
     req.logout((err) => {
         if (err) {
             return next(err);
         }
         req.flash("success", "You are logged out now!");
-        res.redirect("/listings");
+        return res.redirect("/listings");
     });
 };
