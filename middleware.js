afec5a47a50b4f3868bdef142bbe61f6e93b47ad
RamanTomar90 <raman50203@gmail.com>
2026-04-20 18:09:19 +0530

    fix: resolve middleware crash and headers error

--- middleware.js
+++ middleware.js
@@ -1,73 +1,82 @@
 const Listing = require("./models/listing");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema,reviewSchema} = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
-
-
-
-
-module.exports.isLoggedIn = (req,res,next)=>{
-    if(!req.isAuthenticated()){ 
-      req.session.redirectUrl = req.originalUrl;
-    req.flash("error","you must be logged in to create listing!");
+// LOGIN CHECK
+module.exports.isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    req.session.redirectUrl = req.originalUrl;
+    req.flash("error", "you must be logged in to create listing!");
     return res.redirect("/login");
   }
   next();
-}
+};
 
-module.exports.saveRedirectUrl = (req,res,next) =>{
-  if(req.session.redirectUrl){
+// SAVE REDIRECT URL
+module.exports.saveRedirectUrl = (req, res, next) => {
+  if (req.session.redirectUrl) {
     res.locals.redirectUrl = req.session.redirectUrl;
   }
   next();
-}
+};
 
-
-module.exports.isOwner = async(req, res, next) =>{
-  let {id} = req.params;
+// LISTING OWNER CHECK
+module.exports.isOwner = async (req, res, next) => {
+  let { id } = req.params;
   let listing = await Listing.findById(id);
 
-  if (!listing.owner.equals(res.locals.currUser._id)) {
+  if (!listing) {
+    req.flash("error", "Listing not found");
+    return res.redirect("/listings");
+  }
+
+  if (!listing.owner || !listing.owner.equals(res.locals.currUser._id)) {
     req.flash("error", "you dont have permission to edit");
     return res.redirect(`/listings/${id}`);
   }
 
   next();
-
-}
-
-// validate Listing middleware
-module.exports.validateListing = (req, res, next)=>{
-  let {error} = listingSchema.validate(req.body);
-  if(error){
-    let errMsg = error.details.map((el)=> el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  }else{
-    next();
-  }
 };
 
-// validate Review middleware
+// VALIDATE LISTING
+module.exports.validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
 
-module.exports.validateReview = (req, res, next)=>{
-  let {error} = reviewSchema.validate(req.body);
-  if(error){
-    let errMsg = error.details.map((el)=> el.message).join(",");
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
     throw new ExpressError(400, errMsg);
-  }else{
-    next();
   }
+
+  next();
 };
 
-module.exports.isReviewAuthor = async(req, res, next) =>{
-  let { id,reviewId} = req.params;
+// VALIDATE REVIEW
+module.exports.validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  }
+
+  next();
+};
+
+// REVIEW OWNER CHECK
+module.exports.isReviewAuthor = async (req, res, next) => {
+  let { id, reviewId } = req.params;
   let review = await Review.findById(reviewId);
 
-  if (!review.author.equals(res.locals.currUser._id)) {
-    req.flash("error","you are not the author of this review");
+  if (!review) {
+    req.flash("error", "Review not found");
+    return res.redirect(`/listings/${id}`);
+  }
+
+  if (!review.author || !review.author.equals(res.locals.currUser._id)) {
+    req.flash("error", "you are not the author of this review");
     return res.redirect(`/listings/${id}`);
   }
 
   next();
-}
+};
